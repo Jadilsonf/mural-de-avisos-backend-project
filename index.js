bede2d939ec957797ff4239f3b9a02b95851f2c2
jadilsonf <jadilsonfilho171292@gmail.com>
2022-11-11 15:33:46 -0300

    iniciado o fetch

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
 const PORT = 3000
 const express = require("express")
-const bodyParser = require("body-parser")
+const path = require("path")
+const apiRoute = require('./routes/api')
 
 const app = express()
 
-let posts = [
-    {id:"hsuahsauh",
-     title: "teste do mural",
-     description: "descrição do tste"},
-     {id:"hsuahsauh",
-     title: "teste do mural",
-     description: "descrição do tste"},
-     {id:"hsuahsauh",
-     title: "teste do mural",
-     description: "descrição do tste"},
-]
 
-app.get("/all", (req,res)=>{
-    res.json(JSON.stringify(posts))
-}) 
+app.use('/api', apiRoute)
 
-app.post("/new", bodyParser.json(), (req,res)=>{
-    let id = generateID()
-    let title = req.body.title
-    let description = req.body.description
-
-    posts.push({id,title,description})
-
-    res.send("Post adcionado");
-})
+app.use(express.static(path.join(__dirname, "public")))
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
-
-function generateID(){
-    return Math.random().toString(36).substr(2,9)
-}
---
+++ model/posts.js
@@ -0,0 +1,20 @@
+module.exports = {
+    posts: [
+        {id:"hsuahsauh",
+         title: "teste do mural",
+         description: "descrição do tste"},
+    ],
+    // O getAll é um método própro do javascript
+    getAll(){
+        return this.posts
+    },
+
+    newPost(title,description){
+        this.posts.push({id: generateID(),title,description})
+
+    }
+}
+
+function generateID(){
+    return Math.random().toString(36).substr(2,9)
+}
